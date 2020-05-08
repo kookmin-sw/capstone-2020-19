c7307feef9f751c234ba17eed2dd65c8012b9cea
ghyeon0 <xcm1321@gmail.com>
2020-05-08 16:07:16 +0900

    aws: get isWalk to distinguish walk / fall

--- src/aws/lambda-s3-uploader/lambda_function.py
+++ src/aws/lambda-s3-uploader/lambda_function.py
@@ -4,17 +4,20 @@ import datetime
 import uuid
 
 class Upload:
-    def __init__(self, data):
-        now = datetime.datetime.now()
-        self.file_name = str(now.strftime("%Y-%m-%d-%H:%M:%S"))
-        self.bucket_name = "silverwatch-data"
+    def __init__(self, data, is_walk):
+        # now = datetime.datetime.now()
+        self.is_walk = True if is_walk == 1 else False
+        self.file_name = str(uuid.uuid4()) + ("-walk" if self.is_walk else "-fall")
+        self.walk_bucket = "silverwatch-walk"
+        self.fall_bucket = "silverwatch-fall"
         self.data = data
+        
         
     def upload_file(self):
         print("Upload File")
         s3 = boto3.client('s3')
         try:
-            response = s3.upload_file('/tmp/' + self.file_name, self.bucket_name, self.file_name)
+            response = s3.upload_file('/tmp/' + self.file_name, self.walk_bucket if self.is_walk else self.fall_bucket, self.file_name)
         except Exception as e:
             print(e)
             return False
@@ -30,6 +33,6 @@ class Upload:
         return self.upload_file()
         
 def lambda_handler(event, context):
-    upload = Upload(event["data"])
+    upload = Upload(event["data"], event["isWalk"])
     return upload.main()
 
