06882523215418146566360d7f7ec705a3809c6c
Younjae Oh <younjae@kookmin.ac.kr>
2020-05-12 04:16:39 +0900

    server : change class name

--- src/flask-server/app.py
+++ src/flask-server/app.py
@@ -15,25 +15,36 @@ def ping():
 
 parser = reqparse.RequestParser()
 parser.add_argument('battery', type = int)
-parser.add_argument('gps', type = tuple)
+parser.add_argument('gps', type = tuple) #위도, 경도를 저장해야함
 parser.add_argument('watch_id', type = str)
 
-#시계 번호 추가
-def add_watch_id(watch_id):
-    f = open("watch_id", "a")
-    f.write(watch_id)
-    f.close()
+class Check_write_watch_id(Resource):
+    #시계 번호 추가
+    def add_watch_id(watch_id):
+        f = open("watch_id", "a")
+        f.write(watch_id)
+        f.close()
 
-#시계 고유 번호 중복 여부 확인
-def check_watch_id(watch_id):
-    f = open("watch_id", "r")
-    while(Ture):
-        line = f.readline()
-        if not line: break
-        if watch_id == line:
-            return False
-    add_watch_id(watch_id)
-    return True
+    #시계 고유 번호 중복 여부 확인
+    def check_watch_id(watch_id):
+        f = open("watch_id", "r")
+        while(Ture):
+            line = f.readline()
+            if not line: break
+            if watch_id == line:
+                return False
+        add_watch_id(watch_id)
+        return True
+    
+class Status(Resource):
+    def post(self):
+        return {'status' : 'success'}
+
+
+api.add_resource(Check_write_watch_id, '/watch_id')
+#api.add_resource(check_battery, '/battery')
+#api.add_resource(get_gps, '/gps')
+api.add_resource(Status, '/usr')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(debug=True)
