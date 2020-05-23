8f5d067c10c8505a729220a0c536d2c12a87601e
ghyeon0 <xcm1321@gmail.com>
2020-05-23 14:42:10 +0900

    wearos-app: basic uuid & qr-code generation

---
+++ src/wear-os/silverwatch/app/src/main/java/com/example/silverwatch/CreateQR.java
@@ -0,0 +1,34 @@
+package com.example.silverwatch;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+public class CreateQR extends AppCompatActivity {
+    private ImageView iv;
+    private String text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_qr);
+        Intent intent = getIntent();
+        iv = (ImageView)findViewById(R.id.qrcode);
+        text = intent.getStringExtra("id");
+
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try{
+            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,200,200);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            iv.setImageBitmap(bitmap);
+        }catch (Exception e){}
+    }
+}
--- src/wear-os/silverwatch/app/src/main/java/com/example/silverwatch/MainActivity.java
+++ src/wear-os/silverwatch/app/src/main/java/com/example/silverwatch/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.silverwatch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends WearableActivity {
+import java.util.UUID;
 
+public class MainActivity extends WearableActivity {
+    private Button createQRBtn;
     private TextView mTextView;
 
     @Override
@@ -17,5 +22,16 @@ public class MainActivity extends WearableActivity {
 
         // Enables Always-on
         setAmbientEnabled();
+
+        createQRBtn = (Button) findViewById(R.id.createQR);
+
+        createQRBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, CreateQR.class);
+                UUID one = UUID.randomUUID();
+                intent.putExtra("id", one.toString());
+                startActivity(intent);
+            }
+        });
     }
 }
