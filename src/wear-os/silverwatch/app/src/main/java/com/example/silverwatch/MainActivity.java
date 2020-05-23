5bc9f24fa85659cde7f71cea20cac74517427656
ghyeon0 <xcm1321@gmail.com>
2020-05-23 21:27:16 +0900

    wearos-app: show qr-code when registration process not completed

--- src/wear-os/silverwatch/app/src/main/java/com/example/silverwatch/MainActivity.java
+++ src/wear-os/silverwatch/app/src/main/java/com/example/silverwatch/MainActivity.java
@@ -23,12 +23,15 @@ public class MainActivity extends WearableActivity {
         setAmbientEnabled();
         SQLiteDatabase idDB = this.openOrCreateDatabase("ID", MODE_PRIVATE, null);
 //        idDB.execSQL("DROP TABLE IF EXISTS " + tableName);
-        idDB.execSQL("CREATE TABLE IF NOT EXISTS " + tableName
-                + " (uuid text);");
+        idDB.execSQL("CREATE TABLE IF NOT EXISTS " + tableName + " (uuid text);");
+
+        SQLiteDatabase registerDB = this.openOrCreateDatabase("register", MODE_PRIVATE, null);
+        registerDB.execSQL("CREATE TABLE IF NOT EXISTS result (register integer)");
 
         Cursor c = idDB.rawQuery("SELECT * FROM " + tableName, null);
         int rowCount = c.getCount();
         Log.d("db", "Row Count: " + rowCount);
+
         if (rowCount == 0){
             Intent intent = new Intent(MainActivity.this, ButtonQR.class);
             UUID one = UUID.randomUUID();
@@ -38,10 +41,23 @@ public class MainActivity extends WearableActivity {
             idDB.execSQL("INSERT INTO " + tableName + " VALUES ('" + uuid + "');");
             startActivity(intent);
             finish();
+        }else{
+            c.moveToFirst();
+            Cursor registerCursor = registerDB.rawQuery("SELECT * FROM result", null);
+            int registerRowCount = registerCursor.getCount();
+            if (registerRowCount == 0){
+                String uuid = c.getString(c.getColumnIndex("uuid"));
+                Intent intent = new Intent(MainActivity.this, CreateQR.class);
+
+                intent.putExtra("id", uuid);
+                startActivity(intent);
+                finish();
+            }
+            registerCursor.close();
         }
 
         c.close();
         idDB.close();
-
+        registerDB.close();
     }
 }
