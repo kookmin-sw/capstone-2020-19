5f46bc11def225574a7f426c74adb47a493f4548
Younjae Oh <younjae@kookmin.ac.kr>
2020-05-27 23:33:43 +0900

    push server : MyFireBaseMessagingService.java

--- src/push-server/FCMEample/app/src/main/java/silverwatch/capstone/fcm/MainActivity.java
+++ src/push-server/FCMEample/app/src/main/java/silverwatch/capstone/fcm/MainActivity.java
@@ -4,6 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+//import android.support.annotation.NonNull;
+//import android.support.v4.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ src/push-server/FCMEample/app/src/main/java/silverwatch/capstone/fcm/MyFireBaseMessagingService.java
@@ -0,0 +1,57 @@
+package silverwatch.capstone.fcm;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFireBaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onNewToken(String Token){
+        Log.d("FCM Log", "Refreshed Token : " + Token );
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage){
+        if (remoteMessage.getNotification() != null){
+            Log.d("FCM Log", "알림 메세지 : " + remoteMessage.getNotification().getBody());
+            String messageBody = remoteMessage.getNotification().getBody();
+            String messageTitle = remoteMessage.getNotification().getTitle();
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+            String channelID = "Channel ID";
+            Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channelID)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle(messageTitle)
+                    .setContentText(messageBody)
+                    .setAutoCancel(true)
+                    .setSound(defaultSoundUri)
+                    .setContentIntent(pendingIntent);
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.0){
+                String channelName = "Channel Name";
+                NotificationChannel channel = new NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_HIGH);
+                notificationManager.createNotificationChannel(channel);
+
+            }
+            notificationManager.notify(0, notificationBuilder.build());
+        }
+    }
+}
