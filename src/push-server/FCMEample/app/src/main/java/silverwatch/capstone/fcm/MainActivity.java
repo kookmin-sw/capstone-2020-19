2e364194e9647275b055ad50b96c6b944aba7c6a
Younjae Oh <younjae@kookmin.ac.kr>
2020-05-28 01:28:07 +0900

    push server : solve annotation issue

--- src/push-server/FCMEample/app/src/main/java/silverwatch/capstone/fcm/MainActivity.java
+++ src/push-server/FCMEample/app/src/main/java/silverwatch/capstone/fcm/MainActivity.java
@@ -1,7 +1,7 @@
 package silverwatch.capstone.fcm;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
