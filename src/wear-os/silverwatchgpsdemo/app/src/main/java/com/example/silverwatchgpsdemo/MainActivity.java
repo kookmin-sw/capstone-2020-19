974ce1eea1837037b849768c6458d255fd5cf070
ghyeon0 <xcm1321@gmail.com>
2020-05-28 09:10:36 +0900

    wearos-app: show location information in screen

--- src/wear-os/silverwatchgpsdemo/app/src/main/java/com/example/silverwatchgpsdemo/MainActivity.java
+++ src/wear-os/silverwatchgpsdemo/app/src/main/java/com/example/silverwatchgpsdemo/MainActivity.java
@@ -1,21 +1,90 @@
 package com.example.silverwatchgpsdemo;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.Objects;
 
 public class MainActivity extends WearableActivity {
 
-    private TextView mTextView;
+    private TextView txtResult;
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
-
+        txtResult = (TextView) findViewById(R.id.textView);
+        button1 = (Button)findViewById(R.id.button2);
         // Enables Always-on
         setAmbientEnabled();
+
+        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions( MainActivity.this, new String[] {  android.Manifest.permission.ACCESS_FINE_LOCATION  },
+                            0 );
+                }
+                else{
+//                    assert lm != null;
+                    Location location = Objects.requireNonNull(lm).getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if (location != null) {
+                        String provider = location.getProvider();
+                        double longitude = location.getLongitude();
+                        double latitude = location.getLatitude();
+                        double altitude = location.getAltitude();
+                        txtResult.setText(String.format("위치정보 : %s\n위도 : %s\n경도 : %s\n고도  : %s", provider, longitude, latitude, altitude));
+                    }
+
+
+                    lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                            1000,
+                            1,
+                            gpsLocationListener);
+                    lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                            1000,
+                            1,
+                            gpsLocationListener);
+                }
+            }
+        });
+
     }
+    final LocationListener gpsLocationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+
+            String provider = location.getProvider();
+            double longitude = location.getLongitude();
+            double latitude = location.getLatitude();
+            double altitude = location.getAltitude();
+
+            txtResult.setText(String.format("위치정보 : %s\n위도 : %s\n경도 : %s\n고도  : %s", provider, longitude, latitude, altitude));
+
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        public void onProviderEnabled(String provider) {
+        }
+
+        public void onProviderDisabled(String provider) {
+        }
+    };
 }
