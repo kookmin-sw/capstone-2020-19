8d320eb06bf995757d90983054ae4c6a4fd83542
ghyeon0 <xcm1321@gmail.com>
2020-06-08 22:13:09 +0900

    wearos-app: check register result from server

--- src/wear-os/SilverWatchWear/app/src/main/java/capstone/kookmin/silverwatchwear/IntroActivity.java
+++ src/wear-os/SilverWatchWear/app/src/main/java/capstone/kookmin/silverwatchwear/IntroActivity.java
@@ -2,32 +2,48 @@ package capstone.kookmin.silverwatchwear;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
 
 public class IntroActivity extends WearableActivity {
-
+    SQLiteDatabase registerDB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        registerDB = this.openOrCreateDatabase("register", MODE_PRIVATE, null);
+//        registerDB.execSQL("DROP TABLE IF EXISTS result");
+        registerDB.execSQL("CREATE TABLE IF NOT EXISTS result (register integer)");
 
         IntroThread introThread = new IntroThread(handler);
         introThread.start();
     }
 
+    @SuppressLint("HandlerLeak")
     Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg){
             if (msg.what == 1){
-                // TODO: 현재 등록된 정보 가져와서 등록 액티비티로 넘길지 등록 완료 액티비티로 넘길
-                Intent intent = new Intent(IntroActivity.this, startRegister.class);
-                startActivity(intent);
-                finish();
+                Cursor registerCursor = registerDB.rawQuery("SELECT * FROM result", null);
+                int registerRowCount = registerCursor.getCount();
+                if (registerRowCount == 0) {
+                    Intent intent = new Intent(IntroActivity.this, startRegister.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Intent intent = new Intent(IntroActivity.this, registerFinished.class);
+                    Log.d("Intent", "Reg Completed");
+                    startActivity(intent);
+                }
+                registerCursor.close();
             }
         }
     };
--- src/wear-os/SilverWatchWear/app/src/main/java/capstone/kookmin/silverwatchwear/qrGeneration.java
+++ src/wear-os/SilverWatchWear/app/src/main/java/capstone/kookmin/silverwatchwear/qrGeneration.java
@@ -2,6 +2,7 @@ package capstone.kookmin.silverwatchwear;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -10,6 +11,7 @@ import android.graphics.Point;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.os.Message;
 import android.support.wearable.activity.WearableActivity;
 import android.util.Log;
 import android.view.Display;
@@ -42,8 +44,21 @@ public class qrGeneration extends WearableActivity {
     String inputValue;
     Bitmap bitmap;
     QRGEncoder qrgEncoder;
-    RequestQueue requestQueue;
+    SQLiteDatabase registerDB;
+
     boolean registered = false;
+
+    @SuppressLint("HandlerLeak")
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg){
+            if (msg.what == 1){
+                registerDB.execSQL("INSERT INTO result VALUES (1)");
+                Intent intent = new Intent(qrGeneration.this, registerFinished.class);
+                startActivity(intent);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,75 +66,28 @@ public class qrGeneration extends WearableActivity {
         setContentView(R.layout.activity_qr_generation);
 
         initialization();
-    }
 
-    public void waitForRegister(final String uuid){
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                submitData(uuid);
-                Log.d("data", String.valueOf(registered));
-                if (registered) {
-                    Intent intent = new Intent(qrGeneration.this, registerFinished.class);
-                    Log.d("Intent", "Reg Completed");
-
-                    startActivity(intent);
-                    finish();
-                }
-            }
-        }, 15000);
+        Intent intent = getIntent();
+        String uuid = intent.getStringExtra("uuid");
+        registerDB = this.openOrCreateDatabase("register", MODE_PRIVATE, null);
+        generateQRCode(uuid);
+        CheckRegisterThread checkRegisterThread = new CheckRegisterThread(handler, uuid);
+        checkRegisterThread.start();
     }
 
     public void initialization(){
         qrImage = (ImageView) findViewById(R.id.QR_Image);
-        requestQueue = Volley.newRequestQueue(getApplicationContext());
-        final String tableName = "id";
-        SQLiteDatabase idDB = this.openOrCreateDatabase("ID", MODE_PRIVATE, null);
-        SQLiteDatabase registerDB = this.openOrCreateDatabase("register", MODE_PRIVATE, null);
-
-//        idDB.execSQL("DROP TABLE IF EXISTS " + tableName);
-//        registerDB.execSQL("DROP TABLE IF EXISTS result");
-
-        idDB.execSQL("CREATE TABLE IF NOT EXISTS " + tableName + " (uuid text);");
-        registerDB.execSQL("CREATE TABLE IF NOT EXISTS result (register integer)");
-
-        Cursor c = idDB.rawQuery("SELECT * FROM " + tableName, null);
-        int rowCount = c.getCount();
-        Log.d("db", "Row Count: " + rowCount);
-
-        if (rowCount == 0){
-            String uuid = UUID.randomUUID().toString();
-            generateQRCode(uuid);
-            idDB.execSQL("INSERT INTO " + tableName + " VALUES ('" + uuid + "');");
-            waitForRegister(uuid);
-        }else{
-            c.moveToFirst();
-            Cursor registerCursor = registerDB.rawQuery("SELECT * FROM result", null);
-            int registerRowCount = registerCursor.getCount();
-            if (registerRowCount == 0) {
-                String uuid = c.getString(c.getColumnIndex("uuid"));
-                generateQRCode(uuid);
-                waitForRegister(uuid);
-            }else{
-                Intent intent = new Intent(qrGeneration.this, registerFinished.class);
-                Log.d("Intent", "Reg Completed");
-
-                startActivity(intent);
-                finish();
-            }
-            registerCursor.close();
-        }
     }
 
     public void generateQRCode(String inputValue){
         WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        assert manager != null;
         Display display = manager.getDefaultDisplay();
         Point point = new Point();
         display.getSize(point);
         int width = point.x;
         int height = point.y;
-        int smallerDimension = width < height ? width : height;
+        int smallerDimension = Math.min(width, height);
         smallerDimension = smallerDimension * 3 / 4;
 
         qrgEncoder = new QRGEncoder(
@@ -132,41 +100,5 @@ public class qrGeneration extends WearableActivity {
         } catch (WriterException e) {
             Log.v(TAG, e.toString());
         }
-    }
-
-
-    // TODO: Change to POST
-    public void submitData(String data){
-        String url = "http://203.246.112.155:5000/check_watch_id?watch_id="+data;
-
-        // Enter the correct url for your api service site
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.d("data", response.toString());
-                        try {
-                            if (response.getInt("register_result") == 1){
-                                Log.d("data", "registered");
-                                registered = true;
-                                Intent intent = new Intent(qrGeneration.this, registerFinished.class);
-                                Log.d("Intent", "Reg Completed");
-
-                                startActivity(intent);
-                                finish();
-                            }else{
-                                Log.d("data", "not registered");
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        });
-        requestQueue.add(jsonObjectRequest);
     }
 }
