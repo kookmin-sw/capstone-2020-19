df7dfe3e11539a5bac5a4dc1f2658b37ce110d23
ghyeon0 <xcm1321@gmail.com>
2020-06-09 17:55:09 +0900

    push: fcm test

---
+++ src/wear-os/SilverWatchFCMTest/app/src/main/java/capstone/kookmin/silverwatchfcmtest/PushActivity.java
@@ -0,0 +1,26 @@
+package capstone.kookmin.silverwatchfcmtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class PushActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        setContentView(R.layout.activity_push);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        updateUI(currentUser);
+    }
+}
