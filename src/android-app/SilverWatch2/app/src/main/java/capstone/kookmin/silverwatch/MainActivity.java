9716d27f6485c8205d47bb6d432f4cc3d51c0700
reinstate12 <reinstate12@kookmin.ac.kr>
2020-06-09 20:26:24 +0900

    SilverWatch2 push update

--- src/android-app/SilverWatch2/app/src/main/java/capstone/kookmin/silverwatch/MainActivity.java
+++ src/android-app/SilverWatch2/app/src/main/java/capstone/kookmin/silverwatch/MainActivity.java
@@ -5,20 +5,35 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.loader.content.Loader;
 
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.InstanceIdResult;
 import com.pedro.library.AutoPermissions;
 import com.pedro.library.AutoPermissionsListener;
 
 import java.util.ArrayList;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity implements AutoPermissionsListener {
     public static final int REQUEST_CODE_MENU = 101; // REQUEST_CODE_MENU 초기화 (임의 설정 가능)
@@ -81,6 +96,21 @@ public class MainActivity extends AppCompatActivity implements AutoPermissionsLi
                 startActivityForResult(intent, REQUEST_CODE_MENU);
             }
         });
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if(!task.isSuccessful()){
+                            Log.w("FCM Log", "getInstanceID failed", task.getException());
+                            return;
+                        }
+
+                        String token = task.getResult().getToken();
+                        Log.d("FCM Log", "FCM 토큰 : "+ token);
+                        Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     @Override
--- src/android-app/SilverWatch2/app/src/main/java/capstone/kookmin/silverwatch/TracerActivity.java
+++ src/android-app/SilverWatch2/app/src/main/java/capstone/kookmin/silverwatch/TracerActivity.java
@@ -1,18 +1,29 @@
 package capstone.kookmin.silverwatch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import androidx.annotation.Nullable;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
 
-public class TracerActivity extends AppCompatActivity {
+public class TracerActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracer);
+
+        mapView = findViewById(R.id.map_view);
+        mapView.getMapAsync(this);
 
         Button button = findViewById(R.id.button8);
         button.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +34,12 @@ public class TracerActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        Marker marker = new Marker();
+        marker.setPosition(new LatLng(37.5670135, 126.9783740));
+        marker.setMap(naverMap);
     }
 }
